19ac23f88b739f24627251b776bd0a97990ff700
dwd555 <dwd555@qq.com>
2017-10-11 22:56:41 +0800

    增加增删改查

--- MyBatis/src_user/com/dwd/mybatis/inter/IUserOperation.java
+++ MyBatis/src_user/com/dwd/mybatis/inter/IUserOperation.java
@@ -1,7 +1,14 @@
 package com.dwd.mybatis.inter;
 
+import java.util.List;
+
 import com.dwd.mybatis.model.User;
 
+//接口中每个方法名必须与User.xml中的id一致
 public interface IUserOperation {
 	public User selectUserByID(int id);
+	public List<User> selectAllUser();
+	public void insertUser(User user);
+	public void updateUser(User user);
+	public void deleteUser(int id);
 }
--- MyBatis/test_src/com/dwd/test/Test.java
+++ MyBatis/test_src/com/dwd/test/Test.java
@@ -2,6 +2,7 @@ package com.dwd.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -23,13 +24,86 @@ public class Test {
 		}
 		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 	}
-	public static void selectUser() {
+	public static void selectUserByID() {
 		SqlSession session=sqlSessionFactory.openSession();
-		IUserOperation userOperation=session.getMapper(IUserOperation.class);
-		User user=userOperation.selectUserByID(1);
-		System.out.println(user);
+		try {
+			IUserOperation userOperation=session.getMapper(IUserOperation.class);
+			User user=userOperation.selectUserByID(1);
+			System.out.println(user);
+		} finally {
+			session.close();
+		}
+		
 	}
+	public static void selectAllUser() {
+		SqlSession session=sqlSessionFactory.openSession();
+		try {
+			IUserOperation userOperation=session.getMapper(IUserOperation.class);
+			List<User> users=userOperation.selectAllUser();
+			for (User user:users) {
+				System.out.println(user);
+			}
+		} finally {
+			session.close();
+		}
+		
+	}
+	/**
+     * 测试增加,增加后，必须提交事务，否则不会写入到数据库.
+     */
+	public static void insertUser(String userName,String userAge,String userAddress) {
+		User user=new User();
+		user.setUserName(userName);
+		user.setUserAge(userAge);
+		user.setUserAddress(userAddress);
+		SqlSession session=sqlSessionFactory.openSession();
+		try {	
+			IUserOperation userOperation=session.getMapper(IUserOperation.class);
+			userOperation.insertUser(user);
+			session.commit();
+			System.out.println("当前增加的用户 id为:"+user.getId());
+		} finally {
+			session.close();
+		}
+		
+	}
+	/**
+     * 测试修改,修改后，必须提交事务，否则不会写入到数据库.
+     */
+	public static void updateUser() {
+		SqlSession session=sqlSessionFactory.openSession();
+		try {	
+			IUserOperation userOperation=session.getMapper(IUserOperation.class);
+//			先获取id为1的对象,再进行修改
+			User user=userOperation.selectUserByID(1);
+			user.setUserName("dwd111");
+			userOperation.updateUser(user);
+			session.commit();
+		} finally {
+			session.close();
+		}
+	}
+    /**
+     * 删除数据，删除一定要 commit.
+     * @param id
+     */
+    public static void deleteUser(int id){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            IUserOperation userOperation=session.getMapper(IUserOperation.class);           
+            userOperation.deleteUser(id);
+            session.commit();            
+        } finally {
+            session.close();
+        }
+    }
+	
 	public static void main(String[] args) {
-		selectUser();
+//		selectUserByID();
+//		insertUser("dwd333", "26", "chancheng");
+//		updateUser();
+//		deleteUser(1);
+		selectAllUser();
+		
 	}
 }
